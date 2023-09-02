e8077ea85f5d246f32cd457bbc934a5ea933ef3b
SantiagoToloza <santiago.b.toloza@gmail.com>
2023-09-02 16:57:46 -0300

    agregado al auth

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -82,7 +82,7 @@ export const AuthProvider = (props) => {
     }
 
     if (isAuthenticated) {
-      
+
       dispatch({
         type: HANDLERS.INITIALIZE,
       });
@@ -104,10 +104,10 @@ export const AuthProvider = (props) => {
   const signIn = async (email, password) => {
 
     const response = await axios.post('http://localhost:8001/api/auth/login', {
-    email: email,
-    password: password
+      email: email,
+      password: password
     })
- 
+
     try {
       window.sessionStorage.setItem('authenticated', 'true');
     } catch (err) {
@@ -115,10 +115,10 @@ export const AuthProvider = (props) => {
     }
 
     const token = response.data.token
-        const headers = {
-        'Authorization': `Bearer ${token}`
+    const headers = {
+      'Authorization': `Bearer ${token}`
     };
-    const response2 = await axios.get("http://localhost:8001/api/clients/current", { headers } )
+    const response2 = await axios.get("http://localhost:8001/api/clients/current", { headers })
     console.log(response2)
     const user = {
       id: response2.data.id,
