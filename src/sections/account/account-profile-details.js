0b0919a2b890c7dca8c74f5d54c9050c38fe4468
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2023-09-03 00:19:30 -0300

    Termino el register

--- src/sections/account/account-profile-details.js
+++ src/sections/account/account-profile-details.js
@@ -13,7 +13,6 @@ import {
 import axios from 'axios';
 import { useAuth } from 'src/hooks/use-auth';
 
-
 export const AccountProfileDetails = () => {
   const auth = useAuth();
   const [values, setValues] = useState({
@@ -33,18 +32,44 @@ export const AccountProfileDetails = () => {
   );
 
   const handleSubmit = useCallback(
-    (event) => {
+    async (event) => {
       event.preventDefault();
+
+      try {
+        const token = auth.user.token;
+        const headers = {
+          Authorization: `Bearer ${token}`
+        };
+
+        // Los datos actualizados que se enviarán al servidor
+        const updatedData = {
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email
+        };
+
+        // Reemplaza la URL y el método HTTP según lo necesario (PUT o POST)
+        const response = await axios.put(
+          'http://localhost:8001/api/Clients/update',
+          updatedData,
+          { headers }
+        );
+
+        if (response.status === 200) {
+          // La solicitud fue exitosa, puedes manejar la respuesta o redirigir al usuario si es necesario
+          console.log('Datos actualizados con éxito');
+        } else {
+          console.error('Error al actualizar los datos');
+        }
+      } catch (error) {
+        console.error('Error al actualizar el usuario:', error);
+      }
     },
-    []
+    [auth.user.token, values]
   );
 
   return (
-    <form
-      autoComplete="off"
-      noValidate
-      onSubmit={handleSubmit}
-    >
+    <form autoComplete="off" noValidate onSubmit={handleSubmit}>
       <Card>
         <CardHeader
           subheader="The information can be edited"
@@ -52,10 +77,8 @@ export const AccountProfileDetails = () => {
         />
         <CardContent sx={{ pt: 0 }}>
           <Box sx={{ m: -1.5 }}>
-            <Grid
-              container
-              spacing={3}
-            >
+            <Grid container spacing={3}>
+              {/* Tus campos de entrada de datos aquí */}
               <Grid
                 xs={12}
                 md={6}
@@ -96,29 +119,12 @@ export const AccountProfileDetails = () => {
                   value={values.email}
                 />
               </Grid>
-              <Grid
-                xs={12}
-                md={6}
-              >
-               
-              </Grid>
-              <Grid
-                xs={12}
-                md={6}
-              >
-              </Grid>
-              <Grid
-                xs={12}
-                md={6}
-              >
-                
-              </Grid>
             </Grid>
           </Box>
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">
+          <Button variant="contained" type="submit">
             Save details
           </Button>
         </CardActions>
