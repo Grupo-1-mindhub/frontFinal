53b3266518575be58512d83405dcc6434bd3aa91
Axel <Axel@DESKTOP-7H832H0>
2023-09-03 19:30:55 -0300

    Remove signin y register de layout

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -36,22 +36,4 @@ export const items = [
       </SvgIcon>
     )
   },
-  {
-    title: 'Login',
-    path: '/auth/login',
-    icon: (
-      <SvgIcon fontSize="small">
-        <LockClosedIcon />
-      </SvgIcon>
-    )
-  },
-  {
-    title: 'Register',
-    path: '/auth/register',
-    icon: (
-      <SvgIcon fontSize="small">
-        <UserPlusIcon />
-      </SvgIcon>
-    )
-  },
 ];
